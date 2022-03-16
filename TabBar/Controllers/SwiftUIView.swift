bcbde44261efc2db880a421814fc74cb4f11261f
anna-nazarenko <anna.nazarenko.dev@gmail.com>
2022-03-16 18:34:24 +0200

    Add peson icon

--- TabBar/Controllers/SwiftUIView.swift
+++ TabBar/Controllers/SwiftUIView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
+      VStack {
         Text("My name is Anna")
+          .padding()
+        Image(systemName: "person.fill")
+          .padding()
+      }
     }
 }
 
